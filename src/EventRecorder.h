cb9499e6433beb5d4c278383ab3636b229b505a4
Jordan Horwich <jordan@voidingwarranties.com>
2015-03-15 21:54:54 -0700

    Map EventSessions to their corresponding Myo

--- src/EventRecorder.h
+++ src/EventRecorder.h
@@ -28,9 +28,13 @@ class EventRecorder : myo::DeviceListener {
   explicit EventRecorder(EventTypes event_types);
 
   // Call this function after each call to Hub::run or Hub::runOnce.
-  void endEventLoopGroup();
+  void endEventLoopGroup(myo::Myo* myo);
 
-  EventSession getEventSession() const { return events_; }
+  std::map<myo::Myo*, EventSession> getEventSession() const { return events_; }
+  EventSession getEventSession(myo::Myo* myo) const {
+    if (events_.count(myo) == 0) return EventSession();
+    return events_.at(myo);
+  }
 
   ///////////////////////////////////////////////////////////////////////
   // virtual event functions that override functions in DeviceListener //
@@ -61,97 +65,98 @@ class EventRecorder : myo::DeviceListener {
 
  private:
   // Add an event to the current event group.
-  void addEvent(const std::shared_ptr<MyoEvent>& event);
+  void addEvent(myo::Myo* myo, const std::shared_ptr<MyoEvent>& event);
 
   EventTypes event_types_;
-  EventSession events_;
+  std::map<myo::Myo*, EventSession> events_;
 };
 
 EventRecorder::EventRecorder(EventTypes event_types)
     : event_types_(event_types) {}
 
-void EventRecorder::endEventLoopGroup() {
-  events_.events.push_back(EventLoopGroup());
+void EventRecorder::endEventLoopGroup(myo::Myo* myo) {
+  events_[myo].events.push_back(EventLoopGroup());
 }
 
 void EventRecorder::onPair(myo::Myo* myo, uint64_t timestamp,
                            myo::FirmwareVersion firmwareVersion) {
   if (!(event_types_ & PAIR)) return;
-  addEvent(std::make_shared<onPairEvent>(timestamp, firmwareVersion));
+  addEvent(myo, std::make_shared<onPairEvent>(timestamp, firmwareVersion));
 }
 
 void EventRecorder::onUnpair(myo::Myo* myo, uint64_t timestamp) {
   if (!(event_types_ & UNPAIR)) return;
-  addEvent(std::make_shared<onUnpairEvent>(timestamp));
+  addEvent(myo, std::make_shared<onUnpairEvent>(timestamp));
 }
 
 void EventRecorder::onConnect(myo::Myo* myo, uint64_t timestamp,
                               myo::FirmwareVersion firmwareVersion) {
   if (!(event_types_ & CONNECT)) return;
-  addEvent(std::make_shared<onConnectEvent>(timestamp, firmwareVersion));
+  addEvent(myo, std::make_shared<onConnectEvent>(timestamp, firmwareVersion));
 }
 
 void EventRecorder::onDisconnect(myo::Myo* myo, uint64_t timestamp) {
   if (!(event_types_ & DISCONNECT)) return;
-  addEvent(std::make_shared<onDisconnectEvent>(timestamp));
+  addEvent(myo, std::make_shared<onDisconnectEvent>(timestamp));
 }
 
 void EventRecorder::onArmSync(myo::Myo* myo, uint64_t timestamp, myo::Arm arm,
                               myo::XDirection xDirection) {
   if (!(event_types_ & ARM_SYNC)) return;
-  addEvent(std::make_shared<onArmSyncEvent>(timestamp, arm, xDirection));
+  addEvent(myo, std::make_shared<onArmSyncEvent>(timestamp, arm, xDirection));
 }
 
 void EventRecorder::onArmUnsync(myo::Myo* myo, uint64_t timestamp) {
   if (!(event_types_ & ARM_UNSYNC)) return;
-  addEvent(std::make_shared<onArmUnsyncEvent>(timestamp));
+  addEvent(myo, std::make_shared<onArmUnsyncEvent>(timestamp));
 }
 
 void EventRecorder::onUnlock(myo::Myo* myo, uint64_t timestamp) {
   if (!(event_types_ & UNLOCK)) return;
-  addEvent(std::make_shared<onUnlockEvent>(timestamp));
+  addEvent(myo, std::make_shared<onUnlockEvent>(timestamp));
 }
 
 void EventRecorder::onLock(myo::Myo* myo, uint64_t timestamp) {
   if (!(event_types_ & LOCK)) return;
-  addEvent(std::make_shared<onLockEvent>(timestamp));
+  addEvent(myo, std::make_shared<onLockEvent>(timestamp));
 }
 
 void EventRecorder::onPose(myo::Myo* myo, uint64_t timestamp, myo::Pose pose) {
   if (!(event_types_ & POSE)) return;
-  addEvent(std::make_shared<onPoseEvent>(timestamp, pose));
+  addEvent(myo, std::make_shared<onPoseEvent>(timestamp, pose));
 }
 
 void EventRecorder::onOrientationData(myo::Myo* myo, uint64_t timestamp,
                                       const myo::Quaternion<float>& rotation) {
   if (!(event_types_ & ORIENTATION_DATA)) return;
-  addEvent(std::make_shared<onOrientationDataEvent>(timestamp, rotation));
+  addEvent(myo, std::make_shared<onOrientationDataEvent>(timestamp, rotation));
 }
 
 void EventRecorder::onAccelerometerData(myo::Myo* myo, uint64_t timestamp,
                                         const myo::Vector3<float>& accel) {
   if (!(event_types_ & ACCELEROMETER_DATA)) return;
-  addEvent(std::make_shared<onAccelerometerDataEvent>(timestamp, accel));
+  addEvent(myo, std::make_shared<onAccelerometerDataEvent>(timestamp, accel));
 }
 
 void EventRecorder::onGyroscopeData(myo::Myo* myo, uint64_t timestamp,
                                     const myo::Vector3<float>& gyro) {
   if (!(event_types_ & GYROSCOPE_DATA)) return;
-  addEvent(std::make_shared<onGyroscopeDataEvent>(timestamp, gyro));
+  addEvent(myo, std::make_shared<onGyroscopeDataEvent>(timestamp, gyro));
 }
 
 void EventRecorder::onRssi(myo::Myo* myo, uint64_t timestamp, int8_t rssi) {
   if (!(event_types_ & RSSI)) return;
-  addEvent(std::make_shared<onRssiEvent>(timestamp, rssi));
+  addEvent(myo, std::make_shared<onRssiEvent>(timestamp, rssi));
 }
 
 void EventRecorder::onEmgData(myo::Myo* myo, uint64_t timestamp,
                               const int8_t* const emg) {
   if (!(event_types_ & EMG)) return;
-  addEvent(std::make_shared<onEmgDataEvent>(timestamp, emg));
+  addEvent(myo, std::make_shared<onEmgDataEvent>(timestamp, emg));
 }
 
-void EventRecorder::addEvent(const std::shared_ptr<MyoEvent>& event) {
-  events_.events.back().events.push_back(event);
+void EventRecorder::addEvent(myo::Myo* myo,
+                             const std::shared_ptr<MyoEvent>& event) {
+  events_[myo].events.back().events.push_back(event);
 }
 }
