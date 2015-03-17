1190ee42c8cc8f15bef887f62a1eb15644d8756a
Jordan Horwich <jordan@voidingwarranties.com>
2015-03-16 20:48:12 -0700

    Add functions for simulating all Myo events

--- src/SimulatedHub.h
+++ src/SimulatedHub.h
@@ -3,13 +3,13 @@
  * typed.
  */
 
-#ifndef MYOSIMULATOR_SIMULATEDHUB_H_
-#define MYOSIMULATOR_SIMULATEDHUB_H_
+#pragma once
+
+#include <myo/myo.hpp>
 
 #include <iostream>
 #include <string>
 #include <list>
-#include <myo/myo.hpp>
 
 namespace MyoSim {
 class Hub : public myo::Hub {
@@ -39,6 +39,30 @@ class Hub : public myo::Hub {
   // the myo::Hub behavior.
   void run(unsigned int duration_ms) { detectAndTriggerPose(); }
   void runOnce(unsigned int duration_ms) { detectAndTriggerPose(); }
+
+  /////////////////////////////////////////
+  // Functions for simulating Myo events //
+  /////////////////////////////////////////
+  void onPair(myo::Myo* myo, uint64_t timestamp,
+              myo::FirmwareVersion firmware_version);
+  void onUnpair(myo::Myo* myo, uint64_t timestamp);
+  void onConnect(myo::Myo* myo, uint64_t timestamp,
+                 myo::FirmwareVersion firmware_version);
+  void onDisconnect(myo::Myo* myo, uint64_t timestamp);
+  void onArmSync(myo::Myo* myo, uint64_t timestamp, myo::Arm arm,
+                 myo::XDirection x_direction);
+  void onArmUnsync(myo::Myo* myo, uint64_t timestamp);
+  void onUnlock(myo::Myo* myo, uint64_t timestamp);
+  void onLock(myo::Myo* myo, uint64_t timestamp);
+  void onPose(myo::Myo* myo, uint64_t timestamp, const myo::Pose& pose);
+  void onOrientationData(myo::Myo* myo, uint64_t timestamp,
+                         const myo::Quaternion<float>& rotation);
+  void onAccelerometerData(myo::Myo* myo, uint64_t timestamp,
+                           const myo::Vector3<float>& accel);
+  void onGyroscopeData(myo::Myo* myo, uint64_t timestamp,
+                       const myo::Vector3<float>& gyro);
+  void onRssi(myo::Myo* myo, uint64_t timestamp, int8_t rssi);
+  void onEmgData(myo::Myo* myo, uint64_t timestamp, const int8_t* emg);
 
  private:
   void detectAndTriggerPose() {
@@ -75,6 +99,94 @@ class Hub : public myo::Hub {
   std::list<myo::DeviceListener*> listeners_;
   myo::Myo* myo_;
 };
+
+void Hub::onPair(myo::Myo* myo, uint64_t timestamp,
+                 myo::FirmwareVersion firmware_version) {
+  for (auto listener : listeners_) {
+    listener->onPair(myo, timestamp, firmware_version);
+  }
 }
 
-#endif
+void Hub::onUnpair(myo::Myo* myo, uint64_t timestamp) {
+  for (auto listener : listeners_) {
+    listener->onUnpair(myo, timestamp);
+  }
+}
+
+void Hub::onConnect(myo::Myo* myo, uint64_t timestamp,
+                    myo::FirmwareVersion firmware_version) {
+  for (auto listener : listeners_) {
+    listener->onConnect(myo, timestamp, firmware_version);
+  }
+}
+
+void Hub::onDisconnect(myo::Myo* myo, uint64_t timestamp) {
+  for (auto listener : listeners_) {
+    listener->onDisconnect(myo, timestamp);
+  }
+}
+
+void Hub::onArmSync(myo::Myo* myo, uint64_t timestamp, myo::Arm arm,
+                    myo::XDirection x_direction) {
+  for (auto listener : listeners_) {
+    listener->onArmSync(myo, timestamp, arm, x_direction);
+  }
+}
+
+void Hub::onArmUnsync(myo::Myo* myo, uint64_t timestamp) {
+  for (auto listener : listeners_) {
+    listener->onArmUnsync(myo, timestamp);
+  }
+}
+
+void Hub::onUnlock(myo::Myo* myo, uint64_t timestamp) {
+  for (auto listener : listeners_) {
+    listener->onUnlock(myo, timestamp);
+  }
+}
+
+void Hub::onLock(myo::Myo* myo, uint64_t timestamp) {
+  for (auto listener : listeners_) {
+    listener->onLock(myo, timestamp);
+  }
+}
+
+void Hub::onPose(myo::Myo* myo, uint64_t timestamp, const myo::Pose& pose) {
+  for (auto listener : listeners_) {
+    listener->onPose(myo, timestamp, pose);
+  }
+}
+
+void Hub::onOrientationData(myo::Myo* myo, uint64_t timestamp,
+                            const myo::Quaternion<float>& rotation) {
+  for (auto listener : listeners_) {
+    listener->onOrientationData(myo, timestamp, rotation);
+  }
+}
+
+void Hub::onAccelerometerData(myo::Myo* myo, uint64_t timestamp,
+                              const myo::Vector3<float>& accel) {
+  for (auto listener : listeners_) {
+    listener->onAccelerometerData(myo, timestamp, accel);
+  }
+}
+
+void Hub::onGyroscopeData(myo::Myo* myo, uint64_t timestamp,
+                          const myo::Vector3<float>& gyro) {
+  for (auto listener : listeners_) {
+    listener->onGyroscopeData(myo, timestamp, gyro);
+  }
+}
+
+void Hub::onRssi(myo::Myo* myo, uint64_t timestamp, int8_t rssi) {
+  for (auto listener : listeners_) {
+    listener->onRssi(myo, timestamp, rssi);
+  }
+}
+
+void Hub::onEmgData(myo::Myo* myo, uint64_t timestamp, const int8_t* emg) {
+  for (auto listener : listeners_) {
+    listener->onEmgData(myo, timestamp, emg);
+  }
+}
+}
