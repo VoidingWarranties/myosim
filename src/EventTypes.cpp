a640a7243166f4862703d9740212fc639168a73e
Jordan Horwich <jordan@voidingwarranties.com>
2015-03-19 01:41:50 -0700

    Add boost::archive includes
    Although these includes don't seem necessary, they are so as to make the
    BOOST_CLASS_EXPORT macro work properly.

--- src/EventTypes.cpp
+++ src/EventTypes.cpp
@@ -1,5 +1,12 @@
 #include "EventTypes.h"
 
+#include <boost/archive/xml_oarchive.hpp>
+#include <boost/archive/xml_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+
 BOOST_CLASS_EXPORT(MyoSim::onPairEvent);
 BOOST_CLASS_EXPORT(MyoSim::onUnpairEvent);
 BOOST_CLASS_EXPORT(MyoSim::onConnectEvent);
