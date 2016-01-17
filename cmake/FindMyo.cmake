823eeb3ea8b3cffc0d367d4f9f3719730d42c237
Jordan Horwich <jordan@voidingwarranties.com>
2016-01-17 17:54:07 -0500

    Add cmake files
    Closes #8

---
+++ cmake/FindMyo.cmake
@@ -0,0 +1,9 @@
+find_package(PkgConfig)
+
+find_path(Myo_INCLUDE_DIR NAMES myo/myo.hpp)
+find_library(Myo_LIBRARY NAMES myo)
+
+set(Myo_INCLUDE_DIRS ${Myo_INCLUDE_DIR})
+set(Myo_LIBRARIES ${Myo_LIBRARY})
+
+find_package_handle_standard_args(Myo DEFAULT_MSG Myo_INCLUDE_DIR Myo_LIBRARY)
