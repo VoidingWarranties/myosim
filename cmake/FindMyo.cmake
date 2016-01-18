5b8794c6abb88d9afac6fbaecf7dd2ba54225ac9
Jordan Horwich <jordan@voidingwarranties.com>
2016-01-18 13:22:05 -0500

    Add missing include() to FindMyo.cmake

--- cmake/FindMyo.cmake
+++ cmake/FindMyo.cmake
@@ -6,4 +6,5 @@ find_library(Myo_LIBRARY NAMES myo)
 set(Myo_INCLUDE_DIRS ${Myo_INCLUDE_DIR})
 set(Myo_LIBRARIES ${Myo_LIBRARY})
 
+include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Myo DEFAULT_MSG Myo_INCLUDE_DIR Myo_LIBRARY)
